34eb5913109a106f9813a95a3161153891cbd842
Rafael <rafael_breno@outlook.com>
2022-06-26 00:57:06 -0300

    feat: Add separators

--- pkg/parser/tag/struct.go
+++ pkg/parser/tag/struct.go
@@ -7,26 +7,31 @@ import (
 )
 
 const (
-	yaORMTag = "yaorm"
-	tagSep   = ","
+	yaORMTag    = "yaorm"
+	tagSep      = ","
+	keyValueSep = ":"
 )
 
 type (
 	// ParsedTags is a struct that store all information needed
 	// to get the yaORM tags from a struct
 	ParsedTags struct {
-		parsedSlice []parsedTag
-		rawSlice    []string
-		valueOf     reflect.Value
-		typeOf      reflect.Type
-		Error       error
+		parsedTags map[string][]parsedTag
+		rawSlice   []rawTag
+		valueOf    reflect.Value
+		typeOf     reflect.Type
+		Error      error
 	}
 
 	parsedTag struct {
 		tagType tag
-		key     string
 		value   string
-		valueOf reflect.Value
+	}
+
+	rawTag struct {
+		rawStr string
+		field  reflect.StructField
+		value  reflect.Value
 	}
 )
 
@@ -53,15 +58,18 @@ func (p *ParsedTags) getRawTags() *ParsedTags {
 	if p.Error != nil {
 		return p
 	}
-	p.rawSlice = []string{}
+	p.rawSlice = []rawTag{}
 
 	var field reflect.StructField
 
 	for i := 0; i < p.valueOf.NumField(); i++ {
 		field = p.typeOf.Field(i)
-		p.rawSlice = append(p.rawSlice, field.Tag.Get(yaORMTag))
+		p.rawSlice = append(p.rawSlice, rawTag{
+			rawStr: field.Tag.Get(yaORMTag),
+			field:  field,
+			value:  p.valueOf.Field(i),
+		})
 	}
-
 	return p
 }
 
@@ -70,21 +78,23 @@ func (p *ParsedTags) parseRawTags() *ParsedTags {
 		return p
 	}
 
-	p.parsedSlice = []parsedTag{}
+	p.parsedTags = map[string][]parsedTag{}
 
-	for _, tag := range p.rawSlice {
-		parsed := parsedTag{
-			tagType: strTagMap[tag],
+	for _, wholeTag := range p.rawSlice {
+		wholeTagSlice := strings.Split(wholeTag.rawStr, tagSep)
+		parsedTagSlice := []parsedTag{}
+		for _, tag := range wholeTagSlice {
+			splittedTag := strings.Split(tag, ":")
+			pTag := parsedTag{}
+			if len(splittedTag) == 1 {
+				pTag.tagType = strTagMap[splittedTag[0]]
+			} else {
+				pTag.tagType = strTagMap[splittedTag[0]]
+				pTag.value = splittedTag[1]
+			}
+			parsedTagSlice = append(parsedTagSlice, pTag)
 		}
-		tagSlice := strings.Split(tag, tagSep)
-		if len(tagSlice) == 1 {
-			parsed.key = tagSlice[0]
-		} else {
-			parsed.key = tagSlice[0]
-			parsed.value = tagSlice[1]
-		}
-
-		p.parsedSlice = append(p.parsedSlice, parsed)
+		p.parsedTags[wholeTag.field.Name] = parsedTagSlice
 	}
 
 	return p
--- pkg/parser/tag/struct_test.go
+++ pkg/parser/tag/struct_test.go
@@ -5,6 +5,7 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/fiuskylab/yaorm/model"
 	"github.com/stretchr/testify/require"
 )
 
@@ -27,4 +28,57 @@ func TestParseTags(t *testing.T) {
 		require.EqualError(actualParsedTags.Error, expected.Error.Error())
 	})
 
+	// TODO:
+	// 	Find some way to validate the struct and ignore the pointers
+	//t.Run("Valid Struct", func(t *testing.T) {
+	//})
+
+	m := model.Model{}
+	_ = ParseTags(m)
+	_ = ParsedTags{
+		parsedTags: map[string][]parsedTag{
+			"ID": {
+				{
+					tagType: pk,
+					value:   "",
+				},
+				{
+					tagType: autoincrement,
+					value:   "",
+				},
+			},
+			"UpdatedAt": {
+				{
+					tagType: trigger,
+					value:   "update",
+				},
+			},
+			"CreatedAt": {
+				{
+					tagType: trigger,
+					value:   "create",
+				},
+			},
+		},
+		rawSlice: []rawTag{
+			{
+				rawStr: "pk,autoincrement",
+				field:  reflect.TypeOf(m).Field(0),
+				value:  reflect.ValueOf(m).Field(0),
+			},
+			{
+				rawStr: "trigger:update",
+				field:  reflect.TypeOf(m).Field(1),
+				value:  reflect.ValueOf(m).Field(1),
+			},
+			{
+				rawStr: "trigger:create",
+				field:  reflect.TypeOf(m).Field(2),
+				value:  reflect.ValueOf(m).Field(2),
+			},
+		},
+		valueOf: reflect.ValueOf(m),
+		typeOf:  reflect.TypeOf(m),
+		Error:   nil,
+	}
 }
