5a6fce473358124b665f8323dda91d7c4a6b5aeb
Rafael <rafael_breno@outlook.com>
2022-06-25 23:04:46 -0300

    fix: remove deletedAt field

--- model/model.go
+++ model/model.go
@@ -4,8 +4,7 @@ import "time"
 
 // Model is the base for yaORM
 type Model struct {
-	ID        int       `json:"id" yaorm:"pk,autoincrement:true"`
+	ID        int       `json:"id" yaorm:"pk,autoincrement"`
 	CreatedAt time.Time `json:"created_at" yaorm:"trigger:create"`
 	UpdatedAt time.Time `json:"updated_at" yaorm:"trigger:update"`
-	DeletedAt time.Time `json:"deleted_at" yaorm:"trigger:delete"`
 }
