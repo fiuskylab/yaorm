2e296f40f62c165c3ae6ba79270b594871484149
Rafael <rafael_breno@outlook.com>
2022-06-28 00:43:46 -0300

    feat: Add Columns builder

---
+++ driver/pgsql/columns.go
@@ -0,0 +1,39 @@
+package pgsql
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+type (
+	columns []column
+
+	column struct {
+		name     string
+		dataType string
+	}
+)
+
+func (cs columns) toSliceStr() (slice []string) {
+
+	for _, c := range cs {
+		slice = append(slice, fmt.Sprintf("%s %s", c.name, c.dataType))
+	}
+
+	return
+}
+
+func getColums(t reflect.Type, v reflect.Value) (cs columns) {
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		dataType, _ := dataTypes[field.Type.Kind().String()]
+		cs = append(cs, column{
+			name:     strings.ToLower(field.Name),
+			dataType: dataType,
+		})
+	}
+
+	return
+}
