47d85c2e9193e538f9337f2aedfab902aa4bc092
Rafael <rafael_breno@outlook.com>
2022-06-28 00:49:02 -0300

    fix: Remove spaces

--- driver/pgsql/columns.go
+++ driver/pgsql/columns.go
@@ -16,7 +16,6 @@ type (
 )
 
 func (cs columns) toSliceStr() (slice []string) {
-
 	for _, c := range cs {
 		slice = append(slice, fmt.Sprintf("%s %s", c.name, c.dataType))
 	}
@@ -24,8 +23,7 @@ func (cs columns) toSliceStr() (slice []string) {
 	return
 }
 
-func getColums(t reflect.Type, v reflect.Value) (cs columns) {
-
+func getColums(t reflect.Type) (cs columns) {
 	for i := 0; i < t.NumField(); i++ {
 		field := t.Field(i)
 		dataType, _ := dataTypes[field.Type.Kind().String()]
