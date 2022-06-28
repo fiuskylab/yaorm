41c2b89fb4e1be3a549959b52c7b0cde04d66a79
Rafael <rafael_breno@outlook.com>
2022-06-28 02:00:42 -0300

    feat: Add row func for templatig

--- driver/pgsql/templates.go
+++ driver/pgsql/templates.go
@@ -1,19 +1,24 @@
 package pgsql
 
+import (
+	"strings"
+	"text/template"
+)
+
 const (
-	checkTable = `
-	SELECT EXISTS (
+	checkTable = `SELECT EXISTS (
    SELECT FROM information_schema.tables 
    WHERE  table_schema = $1
    AND    table_name   = $2
-   );
-	`
+   );`
 
-	createTable = `
-	CREATE TABLE IF NOT EXISTS ? (
-		{{range .}}
-			{{println . ","}}
-		{{end}}
-	)
- 	`
+	createTable = `CREATE TABLE IF NOT EXISTS %s.%s (
+		{{buildRows .}}
+	);`
 )
+
+var funcWriteRows = template.FuncMap{
+	"buildRows": func(elems []string) string {
+		return strings.Join(elems, ",\n")
+	},
+}
