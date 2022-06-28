1b1d72a83a288cbf5df117554460b1573943df6e
Rafael <rafael_breno@outlook.com>
2022-06-28 00:49:13 -0300

    feat: Add template for CreateTable

--- driver/pgsql/templates.go
+++ driver/pgsql/templates.go
@@ -4,8 +4,16 @@ const (
 	checkTable = `
 	SELECT EXISTS (
    SELECT FROM information_schema.tables 
-   WHERE  table_schema = '%s'
-   AND    table_name   = '%s'
+   WHERE  table_schema = '?'
+   AND    table_name   = '?'
    );
-	 `
+	`
+
+	createTable = `
+	CREATE TABLE IF NOT EXISTS ? (
+		{{range .}}
+			{{println . ","}}
+		{{end}}
+	)
+ 	`
 )
